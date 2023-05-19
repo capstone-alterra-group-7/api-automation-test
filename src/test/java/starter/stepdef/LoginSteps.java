416d1313cb6bcf1a43bdb5bd8939ab59a8833517
nisaauzz <auliaclaudia@upi.edu>
2023-05-19 18:48:43 +0700

    api framework serenity

---
+++ src/test/java/starter/stepdef/LoginSteps.java
@@ -0,0 +1,24 @@
+package starter.stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+    @Given("user has endpoint post new user login")
+    public void userHasEndpointPostNewUserLogin() {
+    }
+
+    @When("user send request for create new user login")
+    public void userSendRequestForCreateNewUserLogin() {
+    }
+
+    @Then("login user get status code {int}")
+    public void loginUserGetStatusCode(int arg0) {
+    }
+
+    @And("I receive valid data for new login")
+    public void iReceiveValidDataForNewLogin() {
+    }
+}
