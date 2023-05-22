ac558c600401258381da69b97ff8f256149ebf02
nisaauzz <auliaclaudia@upi.edu>
2023-05-22 21:45:04 +0700

    negative test login

--- src/test/java/starter/stepdef/LoginSteps.java
+++ src/test/java/starter/stepdef/LoginSteps.java
@@ -11,6 +11,13 @@ public class LoginSteps {
 
     @Steps
     LoginPage login;
+
+    @Steps
+    LoginPage invalidEndpointLogin;
+
+    @Steps
+    LoginPage invalidRequestLogin;
+
     @Given("user has endpoint post new user login")
     public void userHasEndpointPostNewUserLogin() {
         login.setEndpointLogin();
@@ -29,5 +36,33 @@ public class LoginSteps {
     @And("I receive valid data for new login")
     public void iReceiveValidDataForNewLogin() {
         login.validateDataLogin();
+    }
+
+    @Given("user has invalid endpoint post new user login")
+    public void userHasInvalidEndpointPostNewUserLogin() { invalidEndpointLogin.setInvalidEndpointLogin();
+    }
+
+    @When("user send request to invalid endpoint for create new user login")
+    public void userSendRequestToInvalidEndpointForCreateNewUserLogin() { invalidEndpointLogin.sendRequestToInvalidEndpoint();
+    }
+
+    @Then("user get error status code {int}")
+    public void userGetErrorStatusCode(int arg0) { invalidEndpointLogin.validateErrorStatus();
+    }
+
+    @Given("user has valid endpoint post new user login")
+    public void userHasValidEndpointPostNewUserLogin() { invalidRequestLogin.setValidEndpoint();
+    }
+
+    @When("user send invalid request to for create new user login")
+    public void userSendInvalidRequestToForCreateNewUserLogin() { invalidRequestLogin.sendInvalidRequest();
+    }
+
+    @Then("user see error status code {int}")
+    public void userSeeErrorStatusCode(int arg0) { invalidRequestLogin.validateErrorCode();
+    }
+
+    @And("I receive valid response message")
+    public void iReceiveValidResponseMessage() { invalidRequestLogin.validateResponseMessage();
     }
 }
