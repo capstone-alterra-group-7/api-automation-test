1babf0544bcae0f350b674499310abbffa9ba7e3
auliacld <auliaclaudia@upi.edu>
2023-06-03 23:42:45 +0700

    refactor and testing get article by ID automated

--- src/test/java/starter/stepdef/Admin/Article/GetAllArticleSteps.java
+++ src/test/java/starter/stepdef/Admin/Article/GetAllArticleSteps.java
@@ -49,4 +49,52 @@ public class GetAllArticleSteps {
     @And("i validate data with error message response")
     public void iValidateDataWithErrorMessageResponse() { getAllArticles.seeResponseError();
     }
+
+    @When("user has endpoint to get article by id")
+    public void userHasEndpointToGetArticleById() { getAllArticles.setEndpointArticleByID();
+    }
+
+    @And("user send request to endpoint article by id")
+    public void userSendRequestToEndpointArticleById() { getAllArticles.sendReqToEndpointArticleByID();
+    }
+
+    @Then("user get detail article by id with status code {int}")
+    public void userGetDetailArticleByIdWithStatusCode(int arg0) { getAllArticles.validateStatusCodeArticleByID();
+    }
+
+    @And("i validate data article by id with message response")
+    public void iValidateDataArticleByIdWithMessageResponse() { getAllArticles.validateDataWithMessageResponse();
+    }
+
+    @When("user has endpoint to get article with invalid id")
+    public void userHasEndpointToGetArticleWithInvalidId() { getAllArticles.setEndpointArticleInvalidID();
+    }
+
+    @And("user send request to endpoint article with invalid id")
+    public void userSendRequestToEndpointArticleWithInvalidId() { getAllArticles.sendReqToEndpointArticleWithInvalidID();
+    }
+
+    @Then("user cannot get detail article cause invalid id and see status code {int}")
+    public void userCannotGetDetailArticleCauseInvalidIdAndSeeStatusCode(int arg0) { getAllArticles.seeStatusCode();
+    }
+
+    @And("i validate error response")
+    public void iValidateErrorResponse() { getAllArticles.validateErrorResponse();
+    }
+
+    @When("user has invalid endpoint to get article")
+    public void userHasInvalidEndpointToGetArticle() { getAllArticles.setInvalidEndpointByID();
+    }
+
+    @And("user send request to invalid endpoint article")
+    public void userSendRequestToInvalidEndpointArticle() { getAllArticles.sendReqToInvEndpointByID();
+    }
+
+    @Then("user cannot get detail article cause invalid endpoint and see status code {int}")
+    public void userCannotGetDetailArticleCauseInvalidEndpointAndSeeStatusCode(int arg0) { getAllArticles.statusCode();
+    }
+
+    @And("user see error response")
+    public void userSeeErrorResponse() { getAllArticles.seeErrorResponse();
+    }
 }
