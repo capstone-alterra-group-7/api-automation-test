ef6f26fd8e78cb56b6d7fd83710ea7bba9dae57a
auliacld <auliaclaudia@upi.edu>
2023-06-08 13:28:52 +0700

    pengujian endpoint station

---
+++ src/test/java/starter/stepdef/Admin/Station/StationSteps.java
@@ -0,0 +1,46 @@
+package starter.stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import starter.pages.Station;
+
+public class StationSteps {
+
+    @Steps
+    Station station;
+
+
+
+
+    @Given("I set GET api endpoints")
+    public void ISetGetApiEndpoints() {
+      station.setEndpointLogin();
+    }
+
+
+    @When("I request to endpoint for Get All Train")
+    public void iSendGETHTTPRequest() {
+        station.sendRequestLogin();
+    }
+
+    @And("user set GET valid endpoints")
+    public void setPostValidEndpointForGetAllStation() {
+        station.setPostValidEndpointForGetAllStation();
+    }
+
+    @And("user send GET HTTP request with valid endpoint")
+    public void UserSendGetHttpRequestWithValidEndpoint() {
+        station.sendPOSTHTTPRequestForGetAllStation();
+    }
+
+
+    @Then("I receive valid HTTP response code {int}")
+    public void iReceiveValidHTTPResponseCode(int arg0) {
+        station.validateHTTPResponseCode200ForGetAllStation();
+    }
+
+
+}
